20a4ee79c93c6d96ed0f995514bd815ab1c8ee0f
Amanjot-001 <amanjotsingh2309@gmail.com>
2025-01-16 22:11:16 +0530

    solved https://codeforces.com/edu/course/2/lesson/6/1/practice/contest/283911/problem/C

---
+++ basics/binary-search/closest-to-the-right.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n, k;
+	cin >> n >> k;
+	vector<int> a(n);
+	for(int i=0; i<n; i++)
+		cin >> a[i];
+
+	while(k--) {
+		int x;
+		cin >> x;
+
+		int l=-1, r=n;
+		while(l+1<r) {
+			int m = l + ((r-l) >> 1);
+
+			if(a[m] >= x) {
+				r = m;
+			} else {
+				l = m;
+			}
+		}
+
+		cout << r + 1 << endl;
+	}
+
+	return 0;
+}
