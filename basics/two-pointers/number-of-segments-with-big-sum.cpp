23a5b395511052b02a497dd2116e0498eee82146
Amanjot-001 <amanjotsingh2309@gmail.com>
2025-01-28 12:49:27 +0530

    solved https://codeforces.com/edu/course/2/lesson/9/2/practice/contest/307093/problem/D

---
+++ basics/two-pointers/number-of-segments-with-big-sum.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	ll s;
+	cin >> n >> s;
+	vector<int> a(n);
+	for(int i=0; i<n; i++)
+		cin >> a[i];
+
+	ll sum = 0;
+	ll res = 0;
+
+	for(int i=0, j=0; j<n; j++) {
+		sum+=a[j];
+		while(sum-a[i]>=s) {
+			sum-=a[i];
+			i++;
+		}
+
+		if(sum>=s)
+			res += (i+1);
+	}	
+
+	cout << res << endl;
+
+	return 0;
+}
