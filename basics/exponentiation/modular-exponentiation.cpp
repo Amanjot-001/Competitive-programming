a8de38e69a37273a420dc4d49202b891d31dcc81
Amanjot-001 <amanjotsingh2309@gmail.com>
2025-04-07 22:35:52 +0530

    solved https://codeforces.com/problemset/problem/913/A

---
+++ basics/exponentiation/modular-exponentiation.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	#ifndef ONLINE_JUDGE
+		freopen("inputf.txt", "r", stdin);
+		freopen("outputf.txt", "w", stdout);
+	#endif
+
+	int n, m;
+	cin >> n >> m;
+
+	cout << (n>=27 ? m : (m % (1<<n))) << endl;
+
+	return 0;
+}
