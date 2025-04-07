15e503c07cb2709668b9d747790371588ebc3828
Amanjot-001 <amanjotsingh2309@gmail.com>
2025-04-07 21:47:41 +0530

    solved https://www.spoj.com/problems/LASTDIG/

---
+++ basics/exponentiation/the-last-digit.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	#ifndef ONLINE_JUDGE
+		freopen("inputf.txt", "r", stdin);
+		freopen("outputf.txt", "w", stdout);
+	#endif
+
+	int t;
+	cin >> t;
+	while(t--) {
+		ll a, b;
+		cin >> a >> b;
+
+		int m = 10;
+
+		a %= m;
+		ll res = 1;
+		while(b>0) {
+			if(b&1)
+				res = res * a % m;
+			a = a * a % m;
+			b >>= 1;
+		}
+
+		cout << res << endl;
+	}
+
+	return 0;
+}
